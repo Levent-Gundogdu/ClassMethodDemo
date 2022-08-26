a337fcabde26d192dcff3a8d660c46f026e6eadb
leven <levent1061@hotmail.com>
2022-08-26 13:52:57 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Customer added: " + customer);
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Customer deleted: " + customer);
+        }
+
+        public void Print(Customer[] customers)
+        {
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Customer ID: " + customer.Id + "\nCustomer Name: " + customer.Name + " " + customer.Surname + "\nCustomer birth year: " + customer.BirthYear + "\n -----------------------------------------");
+            }
+
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+
+namespace ClassMetotDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+          Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Alex";
+            customer1.Surname = "Walter";
+            customer1.BirthYear = "1993";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Carl";
+            customer2.Surname = "Carlson";
+            customer2.BirthYear = "2000";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.Name = "Max";
+            customer3.Surname = "Donville";
+            customer3.BirthYear = "1998";
+
+            Customer[] customers = { customer1, customer2, customer3};
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.Add(customer3);
+
+
+            customerManager.Delete(customer2);
+
+            customerManager.Print(customers);
+
+
+        }
+    }
+}
